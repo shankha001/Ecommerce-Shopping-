fa131e78969835942b3c2e14a5dac8de3b2bee13
shankha001 <59788600+shankha001@users.noreply.github.com>
2020-06-24 20:08:32 +0530

    update menu-items to navigate withRouter

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import Directory from "../../components/directory/directory.component";
+import React from 'react';
 
-import "./homepage.styles.scss";
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
 const HomePage = () => (
   <div className="homepage">
     <Directory />

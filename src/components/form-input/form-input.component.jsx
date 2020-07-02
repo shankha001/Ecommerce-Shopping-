93e6fb51c1db09716a0daefd0d569cc490a2d5b7
shankha001 <59788600+shankha001@users.noreply.github.com>
2020-07-02 19:27:54 +0530

    finsishing sign in handleSubmit method

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './form-input.styles.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
@@ -6,7 +7,9 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
     <input className="form-input" onChange={handleChange} {...otherProps} />
     {label ? (
       <label
-        className={`${otherProps.value.length}? 'shrink': ''} form-input-label`}
+        className={`${
+          otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}
       >
         {label}
       </label>

030201e09e37b25d39992724ae0e5a30047233ad
shankha001 <shankhasubhrabiswas@gmail.com>
2020-07-19 15:35:43 +0530

    setup addItem to cart redux action

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? 'is-google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} 
+    ${isGoogleSignIn ? 'is-google-sign-in' : ''}   custom-button`}
     {...otherProps}
   >
     {children}

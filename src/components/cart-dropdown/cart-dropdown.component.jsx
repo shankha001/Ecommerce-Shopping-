cfde6257e9eb75aab010ea502be8fe917b3d6f1e
shankha001 <shankhasubhrabiswas@gmail.com>
2020-07-21 22:49:25 +0530

    add empty cart message

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,9 +9,13 @@ import CartItem from '../cart-item/cart-item.component';
 const Cart = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <div className="empty-message">Your cart is empty</div>
+      )}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
